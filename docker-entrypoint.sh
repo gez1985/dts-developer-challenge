2c68ebe941d9a06515acb2b596c823d536f19e87
Gerard Egan <gez.egan@gmail.com>
2025-04-18 10:30:19 +0100

    permissions adde to entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Set correct permissions at container start-up
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
+
 # Run Composer install to install PHP dependencies (only if not installed yet)
 echo "Running Composer install..."
 composer install --no-dev --optimize-autoloader
